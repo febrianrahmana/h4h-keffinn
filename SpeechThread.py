4bb4ecf20293e6a8456014bb1ef4442225a008c0
Febrian Rahman Adi <febrianrahmanadi@gmail.com>
2025-03-23 22:03:16 +0700

    Fixed problems

--- SpeechThread.py
+++ SpeechThread.py
@@ -2,13 +2,22 @@ from PySide6.QtCore import Signal, Slot, Qt, QThread
 from voice_command.speech_processor import SpeechProcessor
 
 class SpeechThread(QThread):
-    def __init__(self, speech_processor: SpeechProcessor):
+    def __init__(self):
         super().__init__()
-        self.speech_processor = speech_processor
+        self._run_flag = False
+        self.speech_processor = SpeechProcessor()
+        
+    def toggle(self):
+        if not self._run_flag:
+            self.run()
+        else:
+            self.stop()
 
     def run(self):
+        self._run_flag = True
         self.speech_processor.listen()
 
     def stop(self):
+        self._run_flag = False
         self.speech_processor.cleanup()
         self.wait()
